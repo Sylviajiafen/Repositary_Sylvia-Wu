08f9ae34bdb2e8ab5630815dd8fb2a3543764142
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-06-24 23:16:30 +0800

    Week 2

--- Week2.playground/Pages/Error Handling in Swift.xcplaygroundpage/Contents.swift
+++ Week2.playground/Pages/Error Handling in Swift.xcplaygroundpage/Contents.swift
@@ -1,20 +1,19 @@
 
-enum GuessNumberGameError {
+enum GuessNumberGameError: Error {
     case wrongNumber
 }
 
 
 class ​GuessNumerGame {
+
+    var targetNumber: Int = 10
     
-    var targetNumber = 10
-    ​
     func guess(number: Int?) throws {
-        
         guard number == targetNumber else {
-          
             throw GuessNumberGameError.wrongNumber
         }
-        
-        print("Guess the right number: ​\(​​targetNumber)​")
-  }
+        print("Guess the right number: ​\(targetNumber)")
+    }
 }
+
+
