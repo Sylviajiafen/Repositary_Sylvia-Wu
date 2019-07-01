9c9ee2f6bacec1cfe33a78be7ac8bf35c1dfe319
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-07-02 02:13:20 +0800

    Week3- not done yet

---
+++ Week3/Week3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Week3
+//
+//  Created by Sylvia Jia Fen  on 2019/6/30.
+//  Copyright © 2019 Sylvia Jia Fen . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginSegmented: UISegmentedControl!
+    @IBOutlet weak var accountText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var checkText: UITextField!
+    @IBOutlet weak var botton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
